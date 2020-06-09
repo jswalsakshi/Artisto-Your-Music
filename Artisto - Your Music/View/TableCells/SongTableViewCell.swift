9e999a02f7d56da1ad96cb629bc59e28fcc556ce
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-06-10 02:49:52 +0530

    first commit

---
+++ Artisto - Your Music/View/TableCells/SongTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SongTableViewCell.swift
+//  Artisto - Your Music
+//
+//  Created by Sakshi Jaiswal on 10/06/20.
+//  Copyright © 2020 Sakshi Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
