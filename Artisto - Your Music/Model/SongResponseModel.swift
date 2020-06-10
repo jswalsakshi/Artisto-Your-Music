a534a7e9882f443c751db43cbb483c44c45ac534
sakshi.jaiswal <sakshi.jaiswal@actolap.com>
2020-06-10 17:28:55 +0530

    updated code

---
+++ Artisto - Your Music/Model/SongResponseModel.swift
@@ -0,0 +1,19 @@
+//
+//  SongResponseModel.swift
+//  Artisto - Your Music
+//
+//  Created by Sakshi Jaiswal on 10/06/20.
+//  Copyright © 2020 Sakshi Jaiswal. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Codable {
+    var results: [Result]?
+}
+
+struct Result: Codable {
+    var trackId: Int?
+    var trackName: String?
+    var collectionName: String?
+}
